de2b36c9ed3d6aa47a9e009ef208cca874cbabbe
Isa Mendes <ismendes@microsoft.com>
2020-04-14 18:09:21 -0700

    Use direct line to authenticate

--- samples/06.recomposing-ui/a.minimizable-web-chat/src/MinimizableWebChat.js
+++ samples/06.recomposing-ui/a.minimizable-web-chat/src/MinimizableWebChat.js
@@ -48,10 +48,15 @@ const MinimizableWebChat = () => {
 
   const handleFetchToken = useCallback(async () => {
     if (!token) {
-      //const res = await fetch('https://webchat-mockbot.azurewebsites.net/directline/token', { method: 'POST' });
-      //const { token } = await res.json();
+        const res = await fetch('https://directline.botframework.com/v3/directline/tokens/generate', {
+            method: 'POST',
+            headers: new Headers({
+                'Authorization': 'Bearer ET1eTs5Lt3E.6IRGoDMbspOsDeheMSxrqrGtOcxLCQ-xJisMS0VSmxw'
+            })
+        });
+      const { token } = await res.json();
 
-      setToken('ET1eTs5Lt3E.6IRGoDMbspOsDeheMSxrqrGtOcxLCQ-xJisMS0VSmxw');
+      setToken(token);
     }
   }, [setToken, token]);
 
